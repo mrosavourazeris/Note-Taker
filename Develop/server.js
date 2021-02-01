7351ed1fd0ddd035a5be839a37bdd610c872da47
Michael Rosa-Vourazeris <mrosavourazeris@gmail.com>
2021-01-31 18:13:23 -0600

    Added a git request to the server.js file so that when the user visits the http://localhost:3000/notes, the notes html in the public folder shows.

--- Develop/server.js
+++ Develop/server.js
@@ -1,13 +1,23 @@
 const path = require('path')
 const express = require('express')
 const fs = require('fs')
+const { notStrictEqual } = require('assert')
 const app = express()
 const PORT = process.env.PORT || 3000
+
 
 
 app.get('/', (req, res) => {
     res.send("You've connected successfully!")
 })
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname + '/public/notes.html'))
+})
+
+// app.get('/api/notes', (req, res) => {
+//     res.sendFile(db/)
+// })
 
 
 
