ae63c49c65c8328743889de2ed5b0f31a55aa0bf
Michael Rosa-Vourazeris <mrosavourazeris@gmail.com>
2021-01-31 18:30:07 -0600

    Added a git request to the server.js file so that when the user visits the http://localhost:3000/*, the index.html in the public folder shows.

--- Develop/server.js
+++ Develop/server.js
@@ -15,6 +15,10 @@ app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname + '/public/notes.html'))
 })
 
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/public/index.html'))
+})
+
 // app.get('/api/notes', (req, res) => {
 //     res.sendFile(db/)
 // })
