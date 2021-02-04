10bf28ddabe31770b4054bd7be06bcdcc5dbb654
Michael Rosa-Vourazeris <mrosavourazeris@gmail.com>
2021-02-03 19:49:00 -0600

    Added a post method so that when the user enters new notes on the webpage, it is saved into the req body, pushed into db.json file, and then written back into the db.json file so it is saved.

--- Develop/server.js
+++ Develop/server.js
@@ -1,10 +1,18 @@
 const path = require('path')
 const express = require('express')
+const http = require('http')
 const fs = require('fs')
 const { notStrictEqual } = require('assert')
 const app = express()
 const PORT = process.env.PORT || 3000
+const dbJson = require('./db/db.json')
+const { json } = require('express')
+const { v4: uuidv4 } = require('uuid')
 
+
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
 
 
 // app.get('/', (req, res) => {
@@ -23,7 +31,14 @@ app.get('/api/notes', (req, res) => {
     res.sendFile(path.join(__dirname + '/db/db.json'))
 })
 
-
+app.post('/api/notes', (req, res) => {
+    const newNote = req.body
+    newNote.id = uuidv4()
+    const allNotes = JSON.parse(fs.readFileSync(path.join(__dirname, '/db/db.json', 'utf8')))
+    allNotes.push(newNote)
+    fs.writeFileSync(path.join(__dirname, '/db/db.json', JSON.stringify(allNotes)))
+    res.sendStatus(200)
+})
 
 app.listen(PORT, () => {
     console.log(`Server listening on http://localhost:${PORT}`)
